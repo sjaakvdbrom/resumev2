152f61e51bba94d6e9c2b1692fb426c42fc09812
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-05-12 22:16:37 +0200

    Add imagekit image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
 import { defineConfig } from "astro/config";
+import { imageService } from "@unpic/astro/service";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  image: {
+    service: imageService({
+      placeholder: "blurhash",
+    }),
+  },
 });
