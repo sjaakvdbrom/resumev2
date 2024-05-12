6a2dada865afc7fb27c33704b549e3150b6b1fa2
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-05-13 00:54:55 +0200

    Add astro icon and replace emoji

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from "astro/config";
 import { imageService } from "@unpic/astro/service";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   image: {
-    service: imageService(),
-  },
+    service: imageService()
+  }
 });
