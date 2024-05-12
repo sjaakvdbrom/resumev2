970fe3c3f415d80886207bb26f1bec5c513c9b44
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-05-12 20:43:36 +0200

    Add unpic image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig, passthroughImageService } from "astro/config";
-
+import { defineConfig } from "astro/config";
+import { imageService } from "@unpic/astro/service";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   image: {
-    service: passthroughImageService(),
+    service: imageService({
+      placeholder: "blurhash",
+    }),
   },
 });
