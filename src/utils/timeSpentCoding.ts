e12cbfaa9090e59825dcb7cac395904122ec93e6
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-06-19 23:25:18 +0200

    Add util for timeSpentCoding

---
+++ src/utils/timeSpentCoding.ts
@@ -0,0 +1,27 @@
+import type { CodingActivityStats } from "@/types/timeSpentCoding";
+
+export const getTimeSpentCoding = async () => {
+  if (!import.meta.env.TIME_SPENT_CODING_URL) {
+    throw new Error("Missing env TIME_SPENT_CODING_URL");
+  }
+
+  if (!import.meta.env.TIME_SPENT_CODING_API_KEY) {
+    throw new Error("Missing env TIME_SPENT_CODING_API_KEY");
+  }
+
+  const response = await fetch(
+    `${import.meta.env.TIME_SPENT_CODING_URL}/stats/last_30_days`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Basic ${import.meta.env.TIME_SPENT_CODING_API_KEY}`,
+      },
+    },
+  );
+  const stats: CodingActivityStats = await response.json();
+
+  const humanReadableTotal =
+    stats.data.human_readable_total_including_other_language;
+
+  return humanReadableTotal;
+};
