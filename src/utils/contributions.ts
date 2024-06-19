0ad2e11e72fef0b76a3ca34eeb7306cad50c517e
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-06-19 23:22:52 +0200

    Add util for contributions

---
+++ src/utils/contributions.ts
@@ -0,0 +1,26 @@
+import type { Response } from "@/types/contributions";
+
+export const getContributions = async () => {
+  if (!import.meta.env.CONTRIBUTIONS_URL) {
+    throw new Error("Missing env CONTRIBUTIONS_URL");
+  }
+
+  const response = await fetch(import.meta.env.CONTRIBUTIONS_URL);
+  const stats: Response = await response.json();
+
+  // Get total contributions from the last 30 days
+  const totalContributions = stats.contributions.reduce((total, item) => {
+    var days = 30;
+    var delta = days * 24 * 60 * 60 * 1000;
+    var result =
+      Math.abs(new Date().valueOf() - new Date(item.date).valueOf()) < delta;
+
+    if (result) {
+      return total + item.count;
+    }
+
+    return total;
+  }, 0);
+
+  return totalContributions;
+};
