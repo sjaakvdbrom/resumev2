865824d3350ec70c0ffb9237af861119d7c04d0c
Sjaak van den Brom <12882171+sjaakvdbrom@users.noreply.github.com>
2024-06-21 21:38:43 +0200

    Add contributions levels

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,13 @@ export default {
       colors: {
         success: "#00dd55",
         warning: "#db6c0a",
+        contributions: {
+          0: "#161b22",
+          1: "#0e4429",
+          2: "#006d32",
+          3: "#26a641",
+          4: "#39d353",
+        },
       },
       spacing: {
         "section-mobile": defaultTheme.spacing[6],
